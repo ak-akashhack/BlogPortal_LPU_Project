d331b0a116bf6144c767dd925f3b43402e2cb5eb
lucky <lucky.laxmanchavan@gmail.com>
2022-09-04 02:09:20 +0530

    Initial commit

---
+++ css/ak.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+// 	double num1;
+//    double num2;
+//    char optr;
+//    cout<<"enter the first number : "<<endl;
+// 	cin>>num1;
+// 	cout<<"enter the oparator : "<<endl;
+// 	cin>>optr;
+//    cout<<"enter the second number : "<<endl;
+//    cin>>num2;
+
+//    switch(optr){
+//    	case '+':cout<<"Addition of the a + b = "<<(num1+num2);
+//       break;
+//       case '-':cout<<"Addition of the a - b = "<<(num1-num2);
+//       break;
+//       case '*':cout<<"Addition of the a * b = "<<(num1*num2);
+//       break;
+//       case '/':cout<<"Addition of the a / b = "<<(num1/num2);
+//       break;
+//       default: cout<<"invalid operator :";
+//    }
+
+
+
+    for (int i = 1; i < 10; i+3)
+    {
+        cout<<i<<endl;
+    }
+    
+
+
+
+
+
+
+   getch();
+}
